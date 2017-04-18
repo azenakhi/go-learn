d9c3cf552753c205c4a431f4510f60aba2605676
azenakhi <ahmed.zenakhi@gmail.com>
2017-04-18 14:07:59 +0200

    Create main.go

---
+++ template/main.go
@@ -0,0 +1,30 @@
+import (
+        "log"
+        "os"
+        "text/template"
+)
+
+type Config struct {
+        Material string
+        Count    uint
+}
+
+func main() {
+        f, err := os.Create("/tmp/config.xml")
+        if err != nil {
+                log.Println("create file: ", err)
+                return
+        }
+        config := Config{"wool", 17}
+        //tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+        tmpl, err := template.ParseFiles("template/config.tmpl")
+
+        if err != nil {
+                panic(err)
+        }
+        err = tmpl.Execute(f, config)
+        if err != nil {
+                panic(err)
+        }
+        f.Close()
+}
