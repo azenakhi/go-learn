751f92b9ef97d3e6709c959a7bff4c0f3846f219
Ahmed ZENAKHI <azenakhi@ahmeds-mbp.home>
2017-04-21 15:22:44 +0200

    Added how to use template and placeholder

--- template/main.go
+++ template/main.go
@@ -1,30 +1,32 @@
+package main
+
 import (
-        "log"
-        "os"
-        "text/template"
+	"log"
+	"os"
+	"text/template"
 )
 
 type Config struct {
-        Material string
-        Count    uint
+	Material string
+	Count    uint
 }
 
 func main() {
-        f, err := os.Create("out/config.xml")
-        if err != nil {
-                log.Println("create file: ", err)
-                return
-        }
-        config := Config{"wool", 17}
-        //tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
-        tmpl, err := template.ParseFiles("tmpl/config.tmpl")
+	f, err := os.Create("out/config.xml")
+	if err != nil {
+		log.Println("create file: ", err)
+		return
+	}
+	config := Config{"wool", 17}
+	//tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+	tmpl, err := template.ParseFiles("tmpl/config.tmpl")
 
-        if err != nil {
-                panic(err)
-        }
-        err = tmpl.Execute(f, config)
-        if err != nil {
-                panic(err)
-        }
-        f.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(f, config)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
 }
