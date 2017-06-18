6bf859d5e59baaa79134815c3bd9595edf593515
azenakhi <azenakhi@ahmeds-mbp.home>
2017-06-18 21:10:11 +0200

    Added architecture structure for REST API project

---
+++ restapi/cmd/restapi/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/azenakhi/go-learn/restapi"
+)
+
+func main() {
+	restapi.Run()
+}
---
+++ restapi/restapi.go
@@ -0,0 +1,14 @@
+package restapi
+
+import (
+	"net/http"
+
+	"log"
+
+	"github.com/azenakhi/go-learn/restapi/routers"
+)
+
+func Run() {
+	userRouter := routers.NewUserRouter()
+	log.Fatal(http.ListenAndServe(":8080", userRouter.GetRouter()))
+}
